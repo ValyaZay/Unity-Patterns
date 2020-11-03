2e92c13a4e27d823c683d057e17ab1f1b27f5ef5
Valya Zaitseva <valentyna.zaitseva@wowkids.io>
2020-11-03 14:48:31 +0200

    Added NullAudioService

---
+++ Assets/MyAssets/Patterns/ServiceLocator/NullAudioService.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace MyAssets.Patterns.ServiceLocator
+{
+    public class NullAudioService : IAudioService
+    {
+        public void PlayLevelSound()
+        {
+            Debug.Log("I'm null object");
+        }
+
+        public void PlayWinSound()
+        {
+            Debug.Log("I'm null object");
+        }
+
+        public void StopLevelSound()
+        {
+            Debug.Log("I'm null object");
+        }
+    }
+}
